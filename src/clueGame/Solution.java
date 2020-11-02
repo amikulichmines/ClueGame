bf82afcabdcfe5bb3d97e755b5fbd19fb3e521a3
amikulichmines <amikulich@mymail.mines.edu>
2020-11-01 21:07:07 -0700

    Refactored and took out useless code

--- src/clueGame/Solution.java
+++ src/clueGame/Solution.java
@@ -14,24 +14,11 @@ public class Solution {
 		return person;
 	}
 
-	public void setPerson(Card person) {
-		this.person = person;
-	}
-
 	public Card getRoom() {
 		return room;
 	}
 
-	public void setRoom(Card room) {
-		this.room = room;
-	}
-
 	public Card getWeapon() {
 		return weapon;
-	}
-
-	public void setWeapon(Card weapon) {
-		this.weapon = weapon;
-	}
-	
+	}	
 }
